9fff126d12d564976b6fbdfa885e9afbb666638d
Dhruv <156397151+DhruvChudasma91@users.noreply.github.com>
2025-12-10 15:17:37 +0530

    feat: add password visibility toggle to input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+
 const Input = ({ 
     label, 
     type, 
@@ -6,6 +9,13 @@ const Input = ({
     onChange,
     required = false
 }) => {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
         <div className="mb-4">
             <label className="text-[13px] text-slate-800 mb-1">
@@ -17,13 +27,21 @@ const Input = ({
                 <input 
                 className="w-full bg-transparent outline-none px-3 py-2 pr-10 border border-gray-300 rounded-md
                 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                type={type} 
+                type={type === 'password' ? (showPassword ? 'text' : 'password') : type} 
                 value={value} 
                 onChange={(e) => onChange(e)}
                 placeholder={placeholder}
                 required={required}
                 />
-
+                {type === 'password' && (
+                    <span className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer">
+                        {showPassword ? (
+                            <Eye size={20} className="text-purple-600" onClick={togglePasswordVisibility}/>
+                        ): (
+                            <EyeOff size={20} className="text-slate-400" onClick={togglePasswordVisibility}/>
+                        )}
+                    </span>
+                )}
             </div>
         
         </div>
