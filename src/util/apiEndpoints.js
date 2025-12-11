768ece77c282372bc898145efecc03c2c37e7137
Dhruv <156397151+DhruvChudasma91@users.noreply.github.com>
2025-12-11 23:01:07 +0530

    feat: add image upload utility for profile pictures via Cloudinary

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,6 +1,8 @@
 export const BASE_URL = "http://localhost:8080/api/v1.0";
 
+const CLOUDINARY_CLOUD_NAME = "dcp8kateo"; 
 export const API_ENDPOINTS = {
     LOGIN: "/login",
-    REGISTER: "/register"
+    REGISTER: "/register",
+    UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`
 }
