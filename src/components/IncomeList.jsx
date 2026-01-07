f3444c422eae41c59699ac78a28c455c32ac38b8
Dhruv <156397151+DhruvChudasma91@users.noreply.github.com>
2026-01-07 12:55:59 +0530

    feat: Income Module Completed with Updates

--- src/components/IncomeList.jsx
+++ src/components/IncomeList.jsx
@@ -1,8 +1,29 @@
-import { Download, Mail } from "lucide-react";
+import { Download, LoaderCircle, Mail } from "lucide-react";
 import TransactionInfoCard from "./TransactionInfoCard";
 import moment from "moment";
+import { useState } from "react";
 
-const IncomeList = ({transactions, onDelete}) => {
+const IncomeList = ({transactions, onDelete, onDownload, onEmail}) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleEmail = async () => {
+    setLoading(true);
+    try {
+      await onEmail();
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const handleDownload = async () => {
+    setLoading(true);
+    try {
+      await onDownload();
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="bg-white rounded-xl shadow border border-gray-100 p-5">
       <div className="flex items-center justify-between">
@@ -13,18 +34,40 @@ const IncomeList = ({transactions, onDelete}) => {
             flex items-center gap-1 px-3 py-1.5 text-sm 
             bg-gray-100 text-gray-700 rounded-lg 
             hover:bg-purple-100 hover:text-purple-700
-            transition-colors duration-200
-          ">
-            <Mail size={15} /> Email
+            transition-colors duration-200"
+            onClick={handleEmail}
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <LoaderCircle className="w-4 h-4 animate-spin" />
+                Emailing...
+              </>
+            ) : (
+              <>
+                <Mail size={15} className="text-base"/> Email
+              </>
+            )}
           </button>
 
           <button className="
             flex items-center gap-1 px-3 py-1.5 text-sm 
             bg-gray-100 text-gray-700 rounded-lg 
             hover:bg-purple-100 hover:text-purple-700
-            transition-colors duration-200
-          ">
-            <Download size={15} /> Download
+            transition-colors duration-200"
+            onClick={handleDownload}
+            disabled={loading}
+          > 
+            {loading ? (
+              <>
+                <LoaderCircle className="w-4 h-4 animate-spin" />
+                Downloading...
+              </>
+            ) : (
+              <>
+                <Download size={15} className="text-base"/> Download
+              </>
+            )}
           </button>
         </div>
       </div>
