e13b5d62c409a31ec8a60df106e1f38f96a26503
Dhruv <156397151+DhruvChudasma91@users.noreply.github.com>
2026-01-09 14:27:12 +0530

    feat : Filter Module completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = ({activeMenu}) => {
     const navigate = useNavigate();
 
     return (
-        <div className="w-64 h-[calc(100vh-64px)] bg-white border-gray-200 sticky top-[61px] p-5 z-20">
+        <div className="w-60 h-[calc(100vh-64px)] bg-white border-gray-200 sticky top-[61px] p-5 z-20">
             <div className="flex flex-col items-center justify-center gap-3 mt-3 mb-7">
                 {user?.profileImageUrl ? (
                     <img src={user?.profileImageUrl || ""} alt="profile image" className="w-20 h-20 bg-slate-400 rounded-full" />
