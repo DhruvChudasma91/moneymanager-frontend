5b9038c9c8a6f866623020773e7052b97ea56060
Dhruv <156397151+DhruvChudasma91@users.noreply.github.com>
2026-01-09 15:09:44 +0530

    feat: Dashboard module updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,73 @@
+import { Coins, Wallet, WalletCards } from "lucide-react";
 import Dashboard from "../components/Dashboard";
+import InfoCard from "../components/InfoCard";
 import { useUser } from "../hooks/useUser";
-
+import {addThousandSeparator} from "../util/util"
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axiosConfig from "../util/axiosConfig";
+import { API_ENDPOINTS } from "../util/apiEndpoints";
+import toast from "react-hot-toast";
 const Home = () => {
   useUser();
+
+  const navigate = useNavigate();
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if(loading) return;
+    setLoading(true);
+
+    try {
+      const response = await axiosConfig.get(API_ENDPOINTS.DASHBOARD_DATA);
+      if(response.status === 200) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.error("Something went wrong while fetching dashboard data:", error);
+      toast.error("Semothing went wrong!");
+    } finally {
+      setLoading(false);
+    }
+
+  }
+
+  useEffect(() => {
+    fetchDashboardData();
+    return () => {};
+  }, []);
+
   return (
     <div>
       <Dashboard activeMenu="Dashboard">
-        This is Home Page
+        <div className="my-5 mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <InfoCard 
+              icon={<WalletCards />}
+              label="Total Balance"
+              value={addThousandSeparator(dashboardData?.totalBalance || 0)}
+              color="bg-purple-800" 
+            />
+            <InfoCard 
+              icon={<Wallet />}
+              label="Total Income"
+              value={addThousandSeparator(dashboardData?.totalIncome || 0)}
+              color="bg-green-800" 
+            />
+            <InfoCard 
+              icon={<Coins />}
+              label="Total Expense"
+              value={addThousandSeparator(dashboardData?.totalExpense || 0)}
+              color="bg-red-800" 
+            />
+          </div>
+
+          <div className="grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+
+          </div>
+
+        </div>
       </Dashboard>
     </div>
   )
